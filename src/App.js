03bdc3ea508d6b07399c810a5eabc712fc9aa9c7
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-11-05 14:42:45 -0500

    implement localStorage hook to persist cart items through refresh

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './data';
 
 import { ProductContext } from "./contexts/ProductContext";
 import { CartContext } from "./contexts/CartContext";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 // Components
 import Navigation from './components/Navigation';
@@ -12,7 +13,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useLocalStorage("cart", []);
 
 	const addItem = item => {
 		// add the given item to the cart
